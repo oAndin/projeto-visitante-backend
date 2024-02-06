335788440c983a7417ab085425385925622e2e65
oAndin <00andherson@gmail.com>
2024-02-05 21:53:51 -0300

    update script

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,15 @@ const DB = require("../database/index");
 
 const table = "users";
 
+async function listAll() {
+  try {
+    const users = await DB.execute(`SELECT * FROM ${table}`);
+    return users;
+  } catch (error) {
+    return { message: error.message };
+  }
+}
+
 async function create(data) {
   // name, password == cpf, profession, gender, age, cep, city, distric
   try {
@@ -9,7 +18,7 @@ async function create(data) {
       throw new Error("Name is required");
     }
     if (!data.user.cpf || data.user.cpf === "") {
-      throw new Error("cpf is required");
+      throw new Error("CPF is required");
     }
     if (!data.user.profession || data.user.profession === "") {
       throw new Error("profession is required");
@@ -29,6 +38,48 @@ async function create(data) {
     if (!data.user.distric || data.user.distric === "") {
       throw new Error("distric is required");
     }
+
+    const alreadyExists = await DB.execute(
+      `SELECT * FROM users WHERE user_cpf = '${data.user.cpf}';`
+    );
+    if (alreadyExists.length > 0) {
+      throw new Error("User already exists");
+    }
+
+    // bycrypt hash must go for cpf and name
+    await DB.execute(
+      `INSERT INTO ${table} (user_name, user_cpf, user_profession, user_gender, user_age, user_cep, user_city, user_distric) VALUES (
+        '${data.user.name}',
+        '${data.user.cpf}',
+        '${data.user.profession}',
+        '${data.user.gender}, 
+        '${data.user.age}', 
+        '${data.user.cep}', 
+        '${data.user.city}', 
+        '${data.user.distric}')`
+    );
+  } catch (error) {
+    message: error.message;
+  }
+}
+
+async function login(data) {
+  try {
+    if (!data.user.name || data.user.name === "") {
+      throw new Error("Name is required");
+    }
+    if (!data.user.cpf || data.user.cpf === "") {
+      throw new Error("CPF is required");
+    }
+    const result = await DB.execute(
+      `SELECT * FROM ${table} WHERE user_name == '${data.user.name}';`
+    );
+    if (result.length === 0) {
+      throw new Error("User or password is incorrect");
+    }
+
+    // bycrypt compare 
+    const response = await 
   } catch (error) {
     message: error.message;
   }
