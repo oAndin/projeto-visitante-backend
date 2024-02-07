30ddac2e7a225205a1681a083696afcf94d1b036
oAndin <00andherson@gmail.com>
2024-02-07 13:33:20 -0300

    update script db

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 async function executeSql(sql) {
   const connection = await mysql.createConnection({
-    host: "",
-    user: "",
-    password: "",
-    database: "",
+    host: "3306",
+    user: "adm",
+    password: "1234",
+    database: "visitante_db",
     port: 3306 || 3007,
   });
   const [result] = await connection.query(sql);
